7a390cbf9c07ce4e5461039a6dafbdbcb17c9658
alinealien <alinebdantunes@gmail.com>
2021-12-01 09:53:13 -0300

    Atualizando aulas: Dominando IDEs Java

---
+++ dominando-ides-java/project_intellij/src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+    }
+
+}
+
+
+        /*int a = 5;
+        int b = 6;
+
+        System.out.println(" Olá mundo cruel... sou eu de novo " + (a+b));
+    }
+}*/
+
+// para importar : alt + enter
+// shift + f6 : refatorar
