98dbedc7b19aa4555f870864e254ac8062912836
alinealien <alinebdantunes@gmail.com>
2021-12-01 11:45:55 -0300

    Finalizando aula: Dominando IDEs

--- dominando-ides-java/project_intellij/src/br/com/dio/model/PrimeiroPrograma.java
+++ dominando-ides-java/project_intellij/src/br/com/dio/model/PrimeiroPrograma.java
@@ -1,18 +1,23 @@
 package br.com.dio;
 
-
 import br.com.dio.model.Gato;
 
-public class PrimeiroPrograma {
+/* public class PrimeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
 
+        Livro livro = new Livro(nome: "A corte de Névoa e Furia", numPaginas: 450);
+        System.out.println(livro);
+
     }
 
+    @Override String toString(){
+    return "Livro{" +
+            "nome='" + nome + '\'' +
+            ", numPaginas=" + numPaginas + '}';
 }
-
 
         /*int a = 5;
         int b = 6;
