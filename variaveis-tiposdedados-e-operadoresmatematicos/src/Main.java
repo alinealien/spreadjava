33777676f199c6edd049b2ca86fdd2d4e9f4e420
alinealien <alinebdantunes@gmail.com>
2021-12-01 21:22:41 -0300

    Finalizando aula: Exercicios

---
+++ variaveis-tiposdedados-e-operadoresmatematicos/src/Main.java
@@ -0,0 +1,50 @@
+public class Main {
+
+    public static void main(String[] args ){
+
+        int i;
+        //int i;
+        int I;
+        //int 1a;
+        int _1a;
+        int $aq;
+
+        i = 5;
+        I = 10;
+        _1a = 20;
+        $aq = 7;
+
+        final int j = 10;
+        //j = 15;
+        int asrn24678md;
+        //int asrn246 78md;
+        //int asrn24678md = 10;
+        //int asrn246%78md = 10;
+
+        asrn24678md = 100;
+        //asrn246%78md = 10;
+
+        int quantidadeProduto = 50;
+        //int QuantidadeProduto;
+        final int NUMERO_TENTATIVAS = 5;
+        //final int numeroTentativas = 5;
+        int QUANTIDADE_OPCOES = 25;
+        int qtdProd;
+
+        System.out.println(i);
+        System.out.println(i);
+        System.out.println(_1a);
+        System.out.println($aq);
+
+        System.out.println(j);
+        System.out.println(asrn24678md);
+        //System.out.println(asrn2$4678md);
+
+        System.out.println(quantidadeProduto);
+        System.out.println(NUMERO_TENTATIVAS);
+        System.out.println(QUANTIDADE_OPCOES);
+
+
+
+    }
+}
