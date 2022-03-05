ceab2bf77436082f4140fec1477311022a5ee713
alinealien <alinebdantunes@gmail.com>
2022-03-05 12:41:56 -0300

    Aula III - Classe Fila / Refatoração para nó Embutido

---
+++ estrutura-de-dados/src/br/com/dio/aulaIV/Main.java
@@ -0,0 +1,24 @@
+package br.com.dio.aulaIV;
+
+public class Main {
+    public static void main(String args[]){
+
+        Fila minhaFila = new Fila();
+
+        minhaFila.enqueue("primeiro");
+        minhaFila.enqueue("segundo");
+        minhaFila.enqueue("terceiro");
+        minhaFila.enqueue("quarto");
+
+        System.out.println(minhaFila);
+
+        System.out.println(minhaFila.dequeue());
+
+        System.out.println(minhaFila);
+
+        minhaFila.enqueue("ultimo");
+
+        System.out.println(minhaFila);
+
+    }
+}
